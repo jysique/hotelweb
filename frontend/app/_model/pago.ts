b72624bf447fc343a133b4e62836108a0cc76b40
brauliosba <50185160+brauliosba@users.noreply.github.com>
2019-06-23 12:56:12 -0500

    Create pago.ts

---
+++ frontend/app/_model/pago.ts
@@ -0,0 +1,10 @@
+import { Reserva } from './reserva';
+
+export class Pago {
+    id: number;
+    fechaPago: string;
+    monto: number;
+    bFormaDePago: boolean;
+    IGV: number;
+    reserva: Reserva;
+}

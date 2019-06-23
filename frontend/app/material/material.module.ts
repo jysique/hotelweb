3e3bb8961766bd24fa8bda108ef503b5426099aa
brauliosba <50185160+brauliosba@users.noreply.github.com>
2019-06-23 13:03:08 -0500

    Create material.module.ts

---
+++ frontend/app/material/material.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+// tslint:disable-next-line: max-line-length
+import { MAT_DATE_LOCALE, MatDatepickerModule, MatSelectModule, MatSnackBarModule, MatButtonModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatMenuModule, MatDividerModule, MatInputModule, MatTableModule, MatCardModule, MatNativeDateModule, MatListModule, MatExpansionModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatInputModule,
+    MatTableModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatListModule,
+    MatExpansionModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatInputModule,
+    MatTableModule,
+    MatSelectModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatListModule,
+    MatExpansionModule
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'es-ES' }
+  ]
+})
+export class MaterialModule { }

f698af39e5ae1e69ea4ae1a7f29b5282c600c7e0
brauliosba <50185160+brauliosba@users.noreply.github.com>
2019-06-23 12:55:29 -0500

    Create reserva.ts

---
+++ frontend/app/_model/reserva.ts
@@ -0,0 +1,9 @@
+import { Cliente } from './cliente';
+
+export class Reserva {
+    public id: number;
+    public fechaEntrada: string;
+    public fechaSalida: string;
+    public fechaConfirmacion: string;
+    public cliente: Cliente;
+}
